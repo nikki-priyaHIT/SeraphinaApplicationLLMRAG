939c7ca8764eb229fc8a57198dc866eb7c42b258
Nikki Priya <nikki.priya.it23@heritageit.edu.in>
2026-06-29 15:53:45 +0530

    Initial commit

---
+++ src/main/java/com/seraphina/seraphinaApplication/SeraphinaApplication.java
@@ -0,0 +1,13 @@
+package com.seraphina.seraphinaApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeraphinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeraphinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seraphina/seraphinaApplication/SeraphinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seraphina.seraphinaApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeraphinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
